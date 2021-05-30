baeaa0ac304f2db3c7d33a24d2a6b1a5541eb427
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-05-30 16:59:16 -0400

    Add icon svg

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
