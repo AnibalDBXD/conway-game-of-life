03d0303d99803ab2dda58c3bff4adf9fa02da17e
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-02 14:01:28 -0400

    Story Cell

---
+++ pages/Home/Table/Cell/Cell.stories.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import Cell from ".";
+import { ICellProps } from "./types";
+
+export default {
+  title: "Pages/Home/Table/Cell",
+  component: Cell,
+  parameters: {
+    docs: {
+      description: {
+        component: "Table's cell"
+      }
+    },
+  }
+} as Meta;
+
+const Template: Story<ICellProps> = (args: ICellProps) => <Cell {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isActive: false,
+};
+
+export const Secondary = Template.bind({});
+Primary.args = {
+  isActive: true,
+};
