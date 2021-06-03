28460903a579e131ee136429604561a9dee8c791
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-03 18:18:07 -0400

    Add data-testid to PlayPause

--- components/PlayPause/index.tsx
+++ components/PlayPause/index.tsx
@@ -1,6 +1,6 @@
 import { StyledButton } from "./styles";
 import { IPlayPause } from "./types";
 
-const PlayPause = ({ onClick, pause }: IPlayPause): JSX.Element => <StyledButton pause={pause} onClick={onClick} />;
+const PlayPause = ({ onClick, pause }: IPlayPause): JSX.Element => <StyledButton data-testid="PlayPauseButton" pause={pause} onClick={onClick} />;
 
 export default PlayPause;
